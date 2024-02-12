7f5f106504e44f1944a69f522b0d7fda3792aedf
BarryThier <barrythiernoousmane326@gmail.com>
2024-02-12 16:20:38 +0000

    Modification du fichier JS

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const appendToDisplay = (value) => {
     document.getElementById('display').value += value;
-}
+};
 const calculate = () => {
     let result = eval(document.getElementById('display').value);
     document.getElementById('display').value = result;
-}
+};
 const appendToClear = () => {
     document.getElementById('display').value = "";
-}
+};
 const squart = () => {
     let value = document.getElementById('display').value;
     if (!isNaN(value) && (value > 0)) {
         document.getElementById('display').value = Math.sqrt(value);
     }
-}
+};
 const power = () => {
     let value = document.getElementById('display').value;
     if(!isNaN(value)) {
        document.getElementById('display').value = Math.pow(value, 2);
     }
-}
+};
 const logarithme = () => {
     let value = document.getElementById('display').value;
     if(!isNaN(value) && (value > 0)) {
@@ -27,4 +27,4 @@ const logarithme = () => {
     }else {
         document.getElementById('display').value = "Error";
     }
-}
+}; 
